4e32a89f0a97a253277d3653b5465c87c6cfd117
Alex Michael Christodoulou Jr <amc8391@rit.edu>
2018-03-09 05:55:01 -0500

    Enabling editing employees' saleries

--- src/UNVHR/Employee.js
+++ src/UNVHR/Employee.js
@@ -179,11 +179,11 @@ export class Employee extends React.Component{
               <div className="infoCard" >
                 <div className="infoStyle" >
                   <label className="label" > Job Title </label>
-                  <input className="inputField" type="text" name="lname" value={this.state.user.jobTitle} placeholder="Assistant Professor" disabled />
+                  <input className="inputField" type="text" name="jobTitle" value={this.state.user.jobTitle} placeholder="Assistant Professor" disabled />
                 </div>
                 <div className="infoStyle" >
                   <label className="label" > Department </label>
-                  <input className="inputField" type="text" name="lname" value={this.state.currentDepartment} placeholder="Software Engineering" disabled />
+                  <input className="inputField" type="text" name="department" value={this.state.currentDepartment} placeholder="Software Engineering" disabled />
                 </div>
                 <label className="label" > Change Department </label>
                 <Select className="infoStyle" Department
@@ -195,11 +195,11 @@ export class Employee extends React.Component{
                 />
                 <div className="infoStyle" >
                   <label className="label" > Salary </label>
-                  <input className="inputField" type="text" name="lname" value={"$" + this.state.user.salary + ".00"} placeholder="$1,000,000.00" disabled />
+                  <input className="inputField" type="text" name="salary" value={this.state.user.salary} placeholder="$1,000,000.00" disabled={(this.state.disabled)? "disabled" : ""} onChange={this.handleChange}/>
                 </div>
                 <div className="infoStyle" >
                   <label className="label" > Salary Estimate </label>
-                  <input className="inputField" type="text" name="lname" value={this.state.salary_estimate} placeholder="$1,000,000.00" disabled />
+                  <input className="inputField" type="text" name="salary_estimate" value={this.state.salary_estimate} placeholder="$1,000,000.00" disabled />
                 </div>
                 <a href="https://www.its.ny.gov">Powered by <img className="nyIMG"  src="https://data.ny.gov/api/assets/24867D9C-004D-4A57-80CA-6757C009D140"></img></a>
               </div>
