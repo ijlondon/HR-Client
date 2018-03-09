48321ef9b29b14b95b50fe145c811b21466acb3a
HuseenMK <hjm3628@rit.edu>
2018-03-09 00:38:17 -0500

    Edit Department name and Head frontedn is doen .. need backend connection

--- src/UNVHR/Departments.js
+++ src/UNVHR/Departments.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './Departments.css';
 import {listDepartments} from './ApiConnector';
 
+
+
 class DepartmentsRow extends React.Component {
+    constructor(props) {
+    super(props)
+    this.state = {
+      contenteditable: true,
+        
+      buttonLabel: "Edit" // inital state
+    }
+    this.enableEdit = this.enableEdit.bind(this);
+  }
+
+  componentDidMount() {
+    this.editButton = ReactDOM.findDOMNode(this.refs.editButton);
+  }
+  
+  
+  enableEdit() {
+    this.setState({
+      contenteditable: !this.state.contenteditable,
+      buttonLabel:"Save" // update it here
+    })
+  }
     render() {
         const {
             data
@@ -10,9 +35,10 @@ class DepartmentsRow extends React.Component {
 
         return ( data.map((data) =>
                 <tr>
-                    <td key={data.name}>{data.name}</td>
+                    <td contenteditable = {(this.state.contenteditable)? "contenteditable" : ""} key={data.name}>{data.name}</td>
+                    <td contenteditable = {(this.state.contenteditable)? "contenteditable" : ""} key={data.head}>{data.headFirst}{" "}{data.headLast}</td>
                     <td key={data.noEmployees} className="empl">{data.noEmployees}</td>
-                    <td><button className="option-button">View</button> <button className="option-button">Edit</button></td>
+                    <td><button className="option-button" >View</button> <button className="option-button" onClick = {this.enableEdit}>{this.state.buttonLabel} </button></td>
                 </tr>)
         );
     }
@@ -31,6 +57,8 @@ export class Departments extends React.Component{
         .then(response => {
             const departments = response.data.map(department => { return {
                 name: department.name,
+                headFirst: department.head.firstName,
+                headLast: department.head.lastName,
                 noEmployees: department.workers.length
             }});
             this.setState({departments: departments})
@@ -46,8 +74,10 @@ export class Departments extends React.Component{
                     <tbody>
                     <tr>
                         <th>Department</th>
+                        <th>Head Name</th>
                         <th>Number of Employees</th>
                         <th>Options</th>
+                        
                     </tr>
                     <DepartmentsRow data={this.state.departments}/>
                     </tbody>
