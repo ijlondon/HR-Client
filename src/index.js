cc905070747b69fbcc79c52d951216972f6fd937
Alex Michael Christodoulou Jr <amc8391@rit.edu>
2018-04-20 13:06:21 -0400

    Locking down routes
    Checking whether the user is logged in before loading routes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,31 @@ import {Login} from "./UNVHR/Login";
 import {Profile} from "./UNVHR/Profile";
 import {Employee} from "./UNVHR/Employee";
 import {Departments} from "./UNVHR/Departments";
+import {getCurrentUser} from './UNVHR/UserService';
 
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.requireAuth = this.requireAuth.bind(this);
+    }
+
+    requireAuth(nextState, replace) {
+        if (!getCurrentUser()) {
+            replace({
+                pathname: '/'
+            })
+        }
+    }
+          
     render(){
         return(
             <Router history={browserHistory}>
                 <Route path={"/"} component={Root}>
                     <IndexRoute component={Home}/>
                     <Route path={"Home"} component={Home}/>
-                    <Route path={"Employee/:employeeId"} component={Employee}/>
-                    <Route path={"Profile"} component={Profile}/>
-                    <Route path={"Departments"} component={Departments}/>
+                    <Route path={"Employee/:employeeId"} component={Employee} onEnter={this.requireAuth}/>
+                    <Route path={"Profile"} component={Profile} onEnter={this.requireAuth}/>
+                    <Route path={"Departments"} component={Departments} onEnter={this.requireAuth}/>
                 </Route>
             </Router>
         );
