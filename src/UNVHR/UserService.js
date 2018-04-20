3047582fe7e9941e1d1aad7dd524225f3119213b
HuseenMK <hjm3628@rit.edu>
2018-04-20 13:30:20 -0400

    Login button doesn't update to logout after loging in [Fixed]

--- src/UNVHR/UserService.js
+++ src/UNVHR/UserService.js
@@ -2,11 +2,13 @@ const userInfoKey = 'loginInfo';
 
 export function handleLogin(response) {
   localStorage.setItem(userInfoKey, JSON.stringify(response));
+  window.location.reload()
 }
 
 export function handleLogout(response) {
   console.log(response);
   localStorage.removeItem(userInfoKey);
+  window.location.reload()
 }
 
 export function getCurrentUser() {
