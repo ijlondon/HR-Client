9172a93869def72280cdf3788d790d949a172fc8
ijlondon <ijl8216@rit.edu>
2018-04-29 19:18:01 -0400

    set upstream

--- src/UNVHR/Departments.js
+++ src/UNVHR/Departments.js
@@ -16,7 +16,7 @@ class DepartmentRow extends React.Component {
     }
     this.toggleEdit = this.toggleEdit.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
-  }
+    }
 
     componentDidMount() {
         this.editButton = ReactDOM.findDOMNode(this.refs.editButton);
@@ -28,15 +28,15 @@ class DepartmentRow extends React.Component {
         this.setState({department: newDepartment});
     }
   
-  toggleEdit() {
-    if (!this.state.contenteditable) {
-        editDepartment(this.state.department);
+    toggleEdit() {
+        if (!this.state.contenteditable) {
+            editDepartment(this.state.department);
+        }
+        this.setState({
+            contenteditable: !this.state.contenteditable,
+            buttonLabel: this.state.contenteditable ? "Save" : "Edit" // update it here
+        })
     }
-    this.setState({
-      contenteditable: !this.state.contenteditable,
-      buttonLabel: this.state.contenteditable ? "Save" : "Edit" // update it here
-    })
-  }
     render() {
         return (
             <tr>
@@ -59,6 +59,15 @@ export class Departments extends React.Component{
         // get the list of departments
         listDepartments() 
         .then(response => { 
+            // const departments = response.data.map((department) => {
+            //     const name = department.name;
+            //     const id = department.id;
+        
+            //     return { id, name }
+            //   });
+            //   this.setState({ departments: departments });
+            // });
+        
             this.setState({departments: response.data});
          })
     }
